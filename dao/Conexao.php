855b065fdfb4bd179ea6e54a549fbe75f0a96baf
Unknown <pauloed443@gmail.com>
2018-06-19 00:37:00 -0300

    Criação a Conexão com o DB
    Criação do arquivo de Conexão com o banco de dados, com sua respectiva classe e functions

---
+++ dao/Conexao.php
@@ -0,0 +1,16 @@
+<?php
+	public class Conexao extends
+	{
+		public static function conexao(){
+			try {
+				$conn = new PDO('mysql:host=localhost;dbname=agenciaturismo', 'root', '');
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$conn->exec("SET CHARACTER SET utf8");
+			} catch (Exception $e) {
+				die("Erro na conexão com o banco de dados: ".$e->getMessage());
+				echo "Erro de conexão na linha ".$e->getLine();
+			}
+			return $conn;
+		}
+	}
+?>
